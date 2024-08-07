f495a54185f2af43b32b7e6ebd6b7175021ad93b
Filipo <filipfilip280@gmail.com>
2024-08-07 04:11:30 +0200

    feat: simplify navigation between sign in and sign up pages by removing tabs and move non dev dependencies to standard dependencies

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,28 +1,24 @@
-import { createFileRoute, Outlet, useLocation } from '@tanstack/react-router';
+import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 import { type ReactNode } from 'react';
 import { AuthLayout } from '@/auth/auth-layout';
+import { Protected } from '@/auth/protected';
 
 export const Route = createFileRoute('/auth')({
   component: Page,
 });
 
-function getCurrentAuthView(pathname: string): 'signUp' | 'signIn' {
-  switch (pathname) {
-    case '/auth/signin':
-      return 'signIn';
-    case '/auth/signup':
-      return 'signUp';
-    default:
-      return 'signIn';
-  }
+function AuthOutlet(): ReactNode {
+  return (
+    <AuthLayout>
+      <Outlet />
+    </AuthLayout>
+  );
 }
 
 function Page(): ReactNode {
-  const { pathname } = useLocation();
-
   return (
-    <AuthLayout currentView={getCurrentAuthView(pathname)}>
-      <Outlet />
-    </AuthLayout>
+    <Protected Fallback={AuthOutlet}>
+      <Navigate to="/app" />
+    </Protected>
   );
 }
