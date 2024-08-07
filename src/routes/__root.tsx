2cc4d727a5cf242b3272c2e33bf31487bdaeb93c
Filipo <filipfilip280@gmail.com>
2024-08-07 04:12:17 +0200

    feat: register user provider and change default query retry to 1

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,26 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { UserProvider } from '@/auth/use-user';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 export const Route = createRootRoute({
   component: () => (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />
-      <TanStackRouterDevtools />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <body className="dark">
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <Outlet />
+          <TanStackRouterDevtools position="top-left" />
+          <ReactQueryDevtools buttonPosition="top-right" />
+        </UserProvider>
+      </QueryClientProvider>
+    </body>
   ),
 });
